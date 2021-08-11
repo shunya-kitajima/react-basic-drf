c17f7e5d0cbec042582f53f12d57cc1566c80371
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2021-08-11 10:49:33 +0900

    axiosでtaskを取得するよう修正

--- src/components/DrfApiFetch.js
+++ src/components/DrfApiFetch.js
@@ -2,6 +2,20 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const DrfApiFetch = () => {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/api/tasks/", {
+        headers: {
+          Authorization: "Token 04cd3ff8431e62b9b6c71dd184959548cf622ce9",
+        },
+      })
+      .then((res) => {
+        setTasks(res.data);
+      });
+  }, []);
+
   return <div></div>;
 };
 
